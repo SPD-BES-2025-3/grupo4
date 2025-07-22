129317d84e59083e941d41c939cc67b19b1c2c51
LeonardoCFilho <leonardo.cortes@discente.ufg.br>
2025-07-22 15:15:53 -0300

    Iniciando implementação de beanie para o mongoDB

---
+++ src/main/python/config/mongo_config.py
@@ -0,0 +1,16 @@
+# python-backend/src/config/mongo_config.py
+
+import motor.motor_asyncio
+from beanie import init_beanie
+from pymongo import MongoClient
+import asyncio
+
+DATABASE_URL = "mongodb://localhost:27017"
+DATABASE_NAME = "ecommerce"
+
+client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URL)
+database = client[DATABASE_NAME]
+
+async def init():
+    # Initialize Beanie ODM with your database models
+    await init_beanie(database, document_models=[Product])
